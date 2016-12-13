7a2b751e8df324ce1a6ffdd54025d6f8f2d016f0
lanouhn <lanouhn@daemon.local>
2016-12-13 19:46:41 +0800

    1.0

---
+++ SetPicture/SetPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SetPicture
+//
+//  Created by lanouhn on 2016/10/19.
+//  Copyright © 2016年 lanouhn06-ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SetPicture/SetPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SetPicture
+//
+//  Created by lanouhn on 2016/10/19.
+//  Copyright © 2016年 lanouhn06-ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
